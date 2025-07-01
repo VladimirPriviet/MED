979006394b3a839b84660700722d864c549da4c6
VladimirPriviet <vladimirpriviet@gmail.com>
2025-07-01 09:47:24 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Paciente, Sintoma, Diagnostico
 
+admin.site.site_header = "Painel Administrativo - MED+"
+admin.site.site_title = "Administração MED+"
+admin.site.index_title = "Bem-vindo ao painel de gestão do sistema MED+"
+
 admin.site.register(Paciente)
 admin.site.register(Sintoma)
 admin.site.register(Diagnostico)
+
